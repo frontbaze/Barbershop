9712c31c0903e526ad8e8cc60aff820684b9d0a4
Anton Vanjukhin <frontbaze@gmail.com>
2022-07-18 18:21:04 +1000

    Добавлена возможность сохранения логина в локальное хранилище

--- js/login.js
+++ js/login.js
@@ -2,14 +2,42 @@ const link = document.querySelector(".login-link");
 const popup = document.querySelector(".modal-login");
 const close = document.querySelector(".modal-close");
 const login = popup.querySelector("[name = login]");
+const password = popup.querySelector("[name = password]");
+const form = popup.querySelector("form");
+const storage = localStorage.getItem("login");
+let isStorageSupport = true;
 
 link.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
-    login.focus();
+    
+    try {
+        if (storage) {
+            login.value = storage;
+        }
+    } catch (err) {
+        isStorageSupport - false;
+    };
+
+    if (storage) {
+        password.focus();
+    } else {
+        login.focus();
+    }
 });
 
 close.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function(evt) {
+    if (!login.value || !password.value) {
+        evt.preventDefault();
+        console.log("Введите логин и пароль");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("login", login.value);
+        }
+    }
 });
