3ca0a3e5136fe925e6628bda526a2fcf285d5017
Anton Vanjukhin <frontbaze@gmail.com>
2022-07-18 15:20:51 +1000

    Добавлен фокус на попап логина

--- js/login.js
+++ js/login.js
@@ -1,10 +1,12 @@
 const link = document.querySelector(".login-link");
 const popup = document.querySelector(".modal-login");
 const close = document.querySelector(".modal-close");
+const login = popup.querySelector("[name = login]");
 
 link.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
+    login.focus();
 });
 
 close.addEventListener("click", function(evt) {
