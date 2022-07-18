13f609bcf1e26ebd351347acce20b525a7231618
Anton Vanjukhin <frontbaze@gmail.com>
2022-07-18 15:13:43 +1000

    Добавлен попап логина и пароля

--- js/login.js
+++ js/login.js
@@ -1,5 +1,13 @@
 const link = document.querySelector(".login-link");
+const popup = document.querySelector(".modal-login");
+const close = document.querySelector(".modal-close");
 
-link.addEventListener("click", function() {
-    console.log("Вы нажали на вход");
+link.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
 });
